7d0da6e72d9b67349e1abd0ff5f04b0e2074068f
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-07-18 11:10:44 +0200

    forgot second

--- src/components/Forgot.js
+++ src/components/Forgot.js
@@ -5,14 +5,15 @@ import {Link} from 'react-router-dom';
 function Forgot() {
 
        //ESTADO inicial
-       const initialState = {
-        email: "",
-        validated: false,
-        sent: false
+        const initialState = {
+            email: "",
+            validated: false,
+            sent: false
     }
 
     //HOOKS
     const [state, setState] = useState(initialState)
+
 
     const handleInput = (event) => {
         setState({
@@ -28,7 +29,6 @@ function Forgot() {
             setState({...state,validated: true, sent: true})
             console.log("email: ", state.email)
         }else{
-            
             console.log("email: ", state.email)
         }
         event.preventDefault()
@@ -38,22 +38,21 @@ function Forgot() {
     if (state.sent===true){
         return (
             <div>
-            <h1>Correo enviado con éxito!!</h1>
-            <Link to="/">Home</Link>
+                <h1>Correo enviado con éxito!!</h1>
+                <Link to="/">Home</Link>
             </div>
         )
             
     }else{
         return (
             <div>
-                <Form onSubmit={inputSave} noValidate validated={state.validated} >
+                <Form onSubmit={inputSave} noValidate validated={state.validated}>
                     <FormGroup>
                         <FormLabel>Email:</FormLabel>
                         <FormControl type="email" name="email" required onChange={handleInput} value={state.email} placeholder='Ingrese su email'/>
                         <Form.Control.Feedback>Tu email ha sido validado!</Form.Control.Feedback>
                         <Form.Control.Feedback type="invalid">Debe insertar un email válido!</Form.Control.Feedback>
                     </FormGroup>
-
                     <Button variant='primary' type="submit">Enviar el nuevo password</Button>
                 </Form>
             </div>
