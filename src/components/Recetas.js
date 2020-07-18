df9248c64a38dd485da2820eab87b9b2d6d55f7f
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-07-18 11:24:27 +0200

    comentarios

--- src/components/Recetas.js
+++ src/components/Recetas.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import Comentarios from './Comentarios'
 
 function Recetas(props) {
     const show = props.recetas.items.map(receta => (
         <h1>{receta.title}</h1>
     ))
+
     return (
         <div>
             <h1>Recetas</h1>
             {show}
+            <Comentarios todosComments={props.comentarios}/>
         </div>
     )
 }
