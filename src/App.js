635a6c03adefde1f43d53f21f7016d5294dbf7ce
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-07-18 11:03:09 +0200

    forgot first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Recetas from './components/Recetas'
