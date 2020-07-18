f558af0fe4543444b6c01710d818689a10b6d600
David de Lamo <d.lamo.valverde@gmail.com>
2020-07-18 12:01:48 +0200

    Add Producto

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,14 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+// function Productos(props) {
+//     const show = props.productos.items.map(producto => (
+//         <h1>{producto.title}</h1>
+//     ))
+
+//     return (
+//         <div>
+//             <h1>Productos</h1>
+//             {show}
+//         </div>
+//     )
+// }
 
 function Productos(props) {
+    const addProduct = (id) => {
+        // props.setCarrito({
+        // })
+    } 
     const show = props.productos.items.map(producto => (
-        <h1>{producto.title}</h1>
+        <div>
+            <img src={producto.img} alt={producto.name}/>
+            <div>
+                <h3>{producto.title}</h3>
+                <p>{producto.description}</p>
+            </div>
+            <button onClick={addProduct(producto.id)}>Add</button>
+        </div>
     ))
-
     return (
         <div>
-            <h1>Productos</h1>
+            <div>
+                <h1>Productos</h1>
+                <Link
+                    to="/newProduct"
+                    >
+                    New Product
+                </Link>
+            </div>
             {show}
+            <Link to="/carrito">Checkout</Link>
         </div>
     )
 }
