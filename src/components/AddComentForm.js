790ca8b4af65cd707bcbda1e6689c41df18580bf
Alba Gustems <albagustems@gmail.com>
2020-07-18 11:04:38 +0200

    Add Comment Form

---
+++ src/components/AddComentForm.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+import {Form, Button} from 'react-bootstrap';
+
+const AddComentForm = (props) => {
+    const initialState = {
+            id: '',
+            idReceta: '1',
+            title: '',
+            description: ''
+    }
+
+    const [newComent, setComent] = useState(initialState)
+    
+    const handleOnChange = (event) => {
+        setComent({
+            ...newComent, 
+            [event.target.name] : event.target.value
+        })
+        
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const commentList = [...props.comentarios.items]
+        commentList.push(newComent);
+        props.setComentariosRecetas({
+            items : commentList,
+        })
+        
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group>
+                <Form.Label>Title</Form.Label>
+                <Form.Control onChange={handleOnChange} type="text" placeholder="Enter title" name="title" value={newComent.title}/>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Description</Form.Label>
+                <Form.Control onChange={handleOnChange} type="text" placeholder="Description" name="description" value={newComent.description}/>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    )
+}
+
+export default AddComentForm
